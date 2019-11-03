fa997fa64076413aa630558e80efc78d2333f1fe
Jay Wen <jaywen89@gmail.com>
2019-11-03 15:19:29 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,6 @@
     second_number != 0 && > 0
     sum = first_number + second_number
     difference = first_number - second_number
+    product = first_number * second_number
+    quotient = 
     
