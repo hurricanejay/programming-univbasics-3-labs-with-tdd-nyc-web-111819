a70dbc271fe87cc93ed57c221e0a09a4c5c28e6a
Jay Wen <jaywen89@gmail.com>
2019-11-03 15:12:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,3 @@
     first_number = 1
     second_number != 0 
-    
-   first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    sum = get_variable_from_file('./calculator.rb', "sum")
-
-    expect(sum).to eq(first_number+second_number)
-  end
+    sum = first_number + second_number
