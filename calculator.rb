2d35f3480a91f228e0a6a0e905c06c24e395d111
Jay Wen <jaywen89@gmail.com>
2019-11-03 15:06:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,6 @@
     first_number = 1
-    second_number != 0 ('./calculator.rb', "second_number")
-
-    expect(second_number).to be_an(Integer).or be_a(Float)
-    expect(second_number).not_to equal(0)
-  end
-  
+    second_number != 0 
+    
    first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     sum = get_variable_from_file('./calculator.rb', "sum")
